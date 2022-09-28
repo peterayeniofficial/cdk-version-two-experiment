e5992a17db73e1b380954b44bdf64d6f29261ccf
Peter Ayeni <ayenicology@gmail.com>
2022-09-28 18:21:30 +0100

    experiement with cdk v2

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,44 @@
+import { HitCounter } from './hitcounter';
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    // define an Aws Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    // define an API Gateway REST API
+    // new apigw.LambdaRestApi(this, 'Endpoint', {
+    //   handler: hello
+    // })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint',{
+      handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      table: helloWithCounter.table,
+      title: 'Hello Hits',
+      sortBy: '-hits'
+      
+    })
+
+   
   }
 }
---
+++ lib/hitcounter.ts
@@ -0,0 +1,35 @@
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import { Construct } from 'constructs'
+
+export interface HitCOunterProps{
+    downstream: lambda.IFunction
+}
+
+export class HitCounter extends Construct{
+    public readonly handler: lambda.Function
+    public readonly table: dynamodb.Table
+    constructor(scope: Construct, id: string, props: HitCOunterProps){
+        super(scope, id)
+
+        this.table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: {name: 'path', type: dynamodb.AttributeType.STRING},
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment:{
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: this.table.tableName
+            }
+        })
+
+        this.table.grantReadWriteData(this.handler)
+        props.downstream.grantInvoke(this.handler)
+    }
+
+}
